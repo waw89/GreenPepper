4557f25c5d063b1b51b6750d9e4cc5eeeb94b3a5
PicosB <brayan.garcia232168@potros.itson.edu.mx>
2024-10-09 01:33:35 -0600

    First Commit

---
+++ src/main/java/com/mycompany/greenpepper/GreenPepper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.greenpepper;
+
+/**
+ *
+ * @author PC
+ */
+public class GreenPepper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
