db43d819b445805b7acbc4ce1b16d076274170cb
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-10-12 02:45:59 -0700

    Menu Created

---
+++ GP-GUI/src/main/java/com/mycompany/gp/gui/Main.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.gp.gui;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Raul
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        deployMenu();
+    }
+ public static void deployMenu() {
+        Scanner tec = new Scanner(System.in);
+        int option = 0;
+
+        do {
+            System.out.println("---------------------------");
+            System.out.println("Welcome to Green Pepper!");
+            System.out.println("Choose an option:");
+            System.out.println("1. Add Order");
+            System.out.println("2. Edit Order");
+            System.out.println("3. Cancel Order");
+            System.out.println("4. View Active Orders");
+            System.out.println("5. View Orders History");
+            System.out.println("6. Exit");
+            System.out.println("---------------------------");
+            
+            System.out.print("I want to: ");
+            option = tec.nextInt(); 
+
+            switch (option) {
+                case 1:
+                    addOrder();
+                    break;
+                case 2:
+                    editOrder();
+                    break;
+                case 3:
+                    cancelOrder();
+                    break;
+                case 4:
+                    viewActiveOrders();
+                    break;
+                case 5:
+                    viewOrderHistory();
+                    break;
+                case 6:
+                    System.out.println("Exiting... Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please choose again.");
+            }
+        } while (option != 6);
+        
+        tec.close(); 
+    }
+
+    public static void addOrder() {
+        System.out.println("Add Order functionality goes here.");
+    }
+
+    public static void editOrder() {
+        System.out.println("Edit Order functionality goes here.");
+    }
+
+    public static void cancelOrder() {
+        System.out.println("Cancel Order functionality goes here.");
+    }
+
+    public static void viewActiveOrders() {
+        System.out.println("Viewing active orders...");
+    }
+
+    public static void viewOrderHistory() {
+        System.out.println("Viewing orders history...");
+    }
+
+}
