3829e2a1b4df2df3417ff094d0cb7dca95c1f633
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-10-15 23:29:24 -0700

    JavaFX implemented

---
+++ GP-GUI/src/main/java/main/MainFX.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Raul
+ */
+public class MainFX extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+           String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ GP-GUI/src/main/java/main/StarterGUI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import com.mycompany.gp.gui.Main;
+
+/**
+ *
+ * @author Raul
+ */
+public class StarterGUI {
+    public static void main(String[] args) {
+        MainFX.main(args);
+    }
+}
