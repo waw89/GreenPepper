464d7e518f4d3fe906f5668dc2fae8787cd54023
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-11-25 23:59:09 -0700

    Admin View Created

---
+++ GP-GUI/src/main/java/controller/ProductsPageController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Raul
+ */
+public class ProductsPageController implements Initializable {
+
+    @FXML
+    private Button btnFood;
+    @FXML
+    private Button btnDrink;
+    @FXML
+    private Button btnExtra;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void showFoods(MouseEvent event) {
+    }
+
+    @FXML
+    private void showDrinks(MouseEvent event) {
+    }
+
+    @FXML
+    private void showExtras(MouseEvent event) {
+    }
+    
+}
