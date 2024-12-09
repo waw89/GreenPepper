dfe0044bc676199d7b72897f34d19d7ecb1c0315
Raúl Luna <jesus.luna235765@potros.itson.edu.mx>
2024-12-09 03:21:49 -0700

    Organization

---
+++ GP-Main/src/main/java/com/mycompany/gp/main/GPMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gp.main;
+
+/**
+ *
+ * @author Raul
+ */
+public class GPMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
